1bdcac6ccc422637483bfd5653b5a81176dd4af7
Lucas César <lucascesar_35@hotmail.com>
2021-06-27 00:32:22 -0300

    First commit: Project created

---
+++ src/main/java/com/example/springbootcdd/SpringbootcddApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootcdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootcddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootcddApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbootcdd/SpringbootcddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootcdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootcddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
