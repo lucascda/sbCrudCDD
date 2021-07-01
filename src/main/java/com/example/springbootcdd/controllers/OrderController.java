e4c67449ff38bdffc1633faefb517453479f3374
Lucas César <lucascesar_35@hotmail.com>
2021-07-01 03:53:41 -0300

    get order by id endpoint

--- src/main/java/com/example/springbootcdd/controllers/OrderController.java
+++ src/main/java/com/example/springbootcdd/controllers/OrderController.java
@@ -1,6 +1,7 @@
 package com.example.springbootcdd.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.example.springbootcdd.controllers.exceptions.ResourceNotFoundException;
 import com.example.springbootcdd.entities.Order;
@@ -9,6 +10,7 @@ import com.example.springbootcdd.repositories.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,4 +30,13 @@ public class OrderController {
         return ResponseEntity.ok().body(list);
     }
     
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Order> getById(@PathVariable Long id){
+        Optional<Order> order = repository.findById(id);
+        if(order == null){
+            throw new ResourceNotFoundException(id);
+        }
+
+        return ResponseEntity.ok().body(order.get());
+    }
 }
